6371efe6d6ebffa689052de4861a0b74edd0a4a3
EddSB <eduardostimamiglio@gmail.com>
2021-10-29 00:34:04 -0300

    ADD: 'data_loader' package to main

--- main.py
+++ main.py
@@ -3,4 +3,9 @@
 Image Classification Project
 """
 
+import data_loader
 
+
+data_filePath = "C:\Users\Pichau\Desktop\TensorFlow\ClassificationProject\Data\test"
+
+data = data_loader(data_filePath)
