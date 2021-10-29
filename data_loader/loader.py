f514dafac2e2d112df202183800f710ba93774d3
EddSB <eduardostimamiglio@gmail.com>
2021-10-29 16:32:01 -0300

    ADD: Function to separate the 'people' superclass from the complete dictionary

--- data_loader/loader.py
+++ data_loader/loader.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Class responsible for import the Data
+Module responsible for importing and reworking Data
 """
 
 import _pickle as pk
@@ -9,3 +9,12 @@ def importData(filePath):
     with open(filePath, 'rb') as fo:
         dict = pk.load(fo, encoding='bytes')
     return dict
+
+def get_supercclass(data_dict, superclass_index):    
+    _result = {'filenames': [], 'data': [], 'fine_labels':[]}
+    for i in range(len(data_dict[b'coarse_labels'])): #
+        if data_dict[b'coarse_labels'][i] == superclass_index :
+            _result['filenames'].append(data_dict[b'filenames'][i])
+            _result['fine_labels'].append(data_dict[b'fine_labels'][i])
+            _result['data'].append(data_dict[b'data'][i].transpose())
+    return _result
--- main.py
+++ main.py
@@ -3,9 +3,21 @@
 Image Classification Project
 """
 
-import data_loader
+from data_loader import loader
 
+#%% Initial parameters
 
 data_filePath = r"C:\Users\Pichau\Desktop\TensorFlow\ClassificationProject\Data\test"
+superclass_index = 14 # 'people' superclass = 14
 
-data = data_loader.loader.importData(data_filePath)
+#%% 
+
+data_dict = loader.importData(data_filePath)
+
+#%% 
+
+people_dict = loader.get_supercclass(superclass_index)
+
+#%%
+
+    
