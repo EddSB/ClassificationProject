8009bc0f8f7cf8567c5ec74890db49a5ef738d40
EddSB <eduardostimamiglio@gmail.com>
2021-10-30 17:05:44 -0300

    ADD: Created a class for the Keras TensorFlow Model

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ Image Classification Project
 """
 
 # import pandas as pd
-# import numpy as np
-from tensorflow import keras
+import numpy as np
+# from tensorflow import keras
 
 from data_handler import handler as dh
 from utilities import visuals
 from configs import constants as const
+from models import tf_models 
 
 
 #%%
@@ -18,6 +19,7 @@ train_dict = dh.importData(const.TRAIN_FILEPATH)
 train_dict = dh.get_superclass(train_dict, const.SUPERCLASS_INDEX)
 train_images = train_dict['data']
 train_images = dh.normalize_pixel_values(train_images)
+train_images = np.array(train_images)
 train_labels = train_dict['fine_labels']
 train_labels = dh.simplify_labels(train_labels, const.FINE_LABEL_NUMBERS)
 
@@ -30,18 +32,16 @@ visuals.display_image(train_images[img_num])
 #%% Building Model
 # I will start using a keras model
 
-model = keras.Sequential([
-    keras.layers.InputLayer(input_shape=(3072)),
-    keras.layers.Dense(128, activation=('relu')),
-    keras.layers.Dense(5, activation=('softmax'))
-    ])
+model = tf_models.Keras_sequential()
+model.build
+
 
 #%% Compiling the Model
 
-model.compile(optimizer='adam',
+model.model.compile(optimizer='adam',
               loss = 'sparse_categorical_crossentropy',
               metrics = ['accuracy'])
 
 #%% Training the Model
 
-# model.fit(train_images, train_labels, epochs = 5)
+model.model.fit(train_images, train_labels, epochs = 30)
--- models/tf_models.py
+++ models/tf_models.py
@@ -6,13 +6,17 @@ Module containing the different applicable TensorFlow models
 from tensorflow import keras
 
 
-class keras_sequential:
+class Keras_sequential:
     
+    model = keras.Sequential()
     
-    model = keras.Sequential([
-        keras.layers.InputLayer(input_shape=(3072)),
-        keras.layers.Dense(1000, activation=('relu')),
-        keras.layers.Dense(300, activation=('relu')),
-        keras.layers.Dense(100, activation=('relu')),
-        keras.layers.Dense(5, activation=('softmax'))
-    ])
+    def build(self):
+        self.model = keras.Sequential([
+            keras.layers.InputLayer(input_shape=(3072)),
+            keras.layers.Dense(1000, activation=('relu')),
+            keras.layers.Dense(300, activation=('relu')),
+            keras.layers.Dense(100, activation=('relu')),
+            keras.layers.Dense(5, activation=('softmax'))
+        ])
+        
+
