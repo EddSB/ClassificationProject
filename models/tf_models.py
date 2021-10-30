a5ebfff9e0f5e0c0489c254399799696824064c1
EddSB <eduardostimamiglio@gmail.com>
2021-10-30 15:56:20 -0300

    ADD: Package to contain all of the applicable TensoFlow models

---
+++ models/tf_models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Module containing the different applicable TensorFlow models
+"""
+
+from tensorflow import keras
+
+
+class keras_sequential:
+    
+    
+    model = keras.Sequential([
+        keras.layers.InputLayer(input_shape=(3072)),
+        keras.layers.Dense(1000, activation=('relu')),
+        keras.layers.Dense(300, activation=('relu')),
+        keras.layers.Dense(100, activation=('relu')),
+        keras.layers.Dense(5, activation=('softmax'))
+    ])
