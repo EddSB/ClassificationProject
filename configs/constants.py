64f3018c5483819a907ce9423e3cbdad475ac556
EddSB <eduardostimamiglio@gmail.com>
2021-10-30 02:10:08 -0300

    ADD: 'constants' module now holds all the constatns to be used troughout the project.

---
+++ configs/constants.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+This module holds all the constant values
+"""
+
+# General Porpouse Constants
+TRAIN_FILEPATH = r"C:\Users\Pichau\Desktop\TensorFlow\ClassificationProject\Data\test"
+SUPERCLASS_INDEX = 14 # 'people' superclass = 14
+CLASS_NAMES = ['baby', 'boy','girl', 'man', 'woman']
+CLASS_NUMBERS = [2, 11, 35, 46, 98]
--- main.py
+++ main.py
@@ -5,31 +5,30 @@ Image Classification Project
 
 from data_handler import handler as dh
 from utilities import visuals
+from configs import constants as const
 
 import pandas as pd
 import numpy as np
 
 
-#%% Initial parameters
-
-train_filePath = r"C:\Users\Pichau\Desktop\TensorFlow\ClassificationProject\Data\test"
-superclass_index = 14 # 'people' superclass = 14
+#%%
+# train_filePath = r"C:\Users\Pichau\Desktop\TensorFlow\ClassificationProject\Data\test"
+# superclass_index = 14 # 'people' superclass = 14
 
 #%%
 
-train_complete_dict = dh.importData(train_filePath)
+train_complete_dict = dh.importData(const.TRAIN_FILEPATH)
 
 #%% 
 
-train_dict = dh.get_superclass(train_complete_dict, superclass_index)
+train_dict = dh.get_superclass(train_complete_dict, const.SUPERCLASS_INDEX)
 train_images = train_dict['data']
+train_labels = train_dict['fine_labels']
 
-#%% WORK IN PROGRESS
 
-# Normalizing Values
+#%% Normalizing Values
 
 dh.normalize_pixel_values(train_images)
-print (dh)
 
 #%% Visualizando imagens
 
