1082faa21d73f5ca6dabccc6139910edf45c57aa
EddSB <eduardostimamiglio@gmail.com>
2021-10-30 09:55:56 -0300

    MOD: Included original label number constant

--- configs/constants.py
+++ configs/constants.py
@@ -3,8 +3,8 @@
 This module holds all the constant values
 """
 
-# General Porpouse Constants
+# General Purpouse Constants
 TRAIN_FILEPATH = r"C:\Users\Pichau\Desktop\TensorFlow\ClassificationProject\Data\test"
 SUPERCLASS_INDEX = 14 # 'people' superclass = 14
 CLASS_NAMES = ['baby', 'boy','girl', 'man', 'woman']
-CLASS_NUMBERS = [2, 11, 35, 46, 98]
+FINE_LABEL_NUMBERS = [2, 11, 35, 46, 98]
