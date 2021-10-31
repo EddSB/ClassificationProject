d4c1b4383f81bfba5d1c6e507bc9b5fd6787a381
EddSB <eduardostimamiglio@gmail.com>
2021-10-31 00:15:31 -0300

    MOD: Fixed tesst and train file paths

--- configs/constants.py
+++ configs/constants.py
@@ -5,9 +5,9 @@ This module holds all the global constant values
 
 # General Purpouse Constants
 TRAIN_FILEPATH = (r"C:\Users\Pichau\Desktop\TensorFlow"
-                  r"\ClassificationProject\data\test")
+                  r"\ClassificationProject\data\train")
 TEST_FILEPATH = (r"C:\Users\Pichau\Desktop\TensorFlow"
-                 r"\ClassificationProject\data\train")
+                 r"\ClassificationProject\data\test")
 SUPERCLASS_INDEX = 14 # 'people' superclass = 14
 CLASS_NAMES = ['baby', 'boy','girl', 'man', 'woman']
 FINE_LABEL_NUMBERS = [2, 11, 35, 46, 98]
