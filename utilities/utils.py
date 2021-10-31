fda4d257f364072e98470e5ba43394f4ca4b5e48
EddSB <eduardostimamiglio@gmail.com>
2021-10-31 10:05:35 -0300

    ADD: Utilities to transform images from RGB to Grayscale and to visualise them

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Different utiliy functions that do not fit in other categories
+Utiliy functions that do not fit in other categories
 """
+
+import numpy as np
+
+
+def rgb_to_gray_scale(rgb_img):
+    """Transforms an images format from rgb to gray scale"""   
+    
+    rgb_img = rgb_img.reshape(3, 32, 32)
+    rgb_img = rgb_img.transpose(1, 2, 0)
+    
+    b = [0.2989, 0.5870, 0.1140]
+    
+    gray_img = np.dot(rgb_img, b)
+    
+    gray_img = gray_img.reshape(1024)
+    
+    return gray_img
--- utilities/visuals.py
+++ utilities/visuals.py
@@ -8,22 +8,32 @@ import matplotlib.pyplot as plt
 import configs.constants as CONST
 
 
-def display_image(image, label):
-    print("Image class: ", CONST.CLASS_NAMES[label])
-    plt.figure()
+def _display_image(image):
+    """Displays an image from RGB image array"""
     image = image.reshape(3,32,32)
     image = image.transpose(1, 2, 0)
+    plt.figure()
     plt.imshow(image)
     plt.show()
     
     
-def comparative(image, true_label, predicted_label):
+def compare_result(index, images, true_labels, predictions):
+    """
+    Displays the image RGB image and prints strings
+    to compare the real label with the predicted label
+    """
+    predicted_label = np.argmax(predictions[index])
     print("Image class: ",
-          CONST.CLASS_NAMES[np.int(true_label)],
+          CONST.CLASS_NAMES[np.int(true_labels[index])],
           ", predicted: ",
           CONST.CLASS_NAMES[predicted_label])
+    _display_image(images[index])
+    
+    
+def display_gray_image(image):
+    """Displays an image from grayscale image array"""
+    image = image.reshape(32,32)
     plt.figure()
-    image = image.reshape(3,32,32)
-    image = image.transpose(1, 2, 0)
-    plt.imshow(image)
+    plt.imshow(image, cmap='gray')
     plt.show()
+    
