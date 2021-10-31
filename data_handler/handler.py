1da965ac6f079668077ecf381542c74781a34b2a
EddSB <eduardostimamiglio@gmail.com>
2021-10-30 23:53:29 -0300

    MOD: Refactored import script to allow the import of testing data

--- data_handler/handler.py
+++ data_handler/handler.py
@@ -6,14 +6,20 @@ Data
 
 import _pickle as pk
 import numpy as np
+import configs.constants as CONST
 
-def importData(filePath):
+def import_data(filePath):
     with open(filePath, 'rb') as fo:
-        dict = pk.load(fo, encoding='bytes')
-    return dict
+        data_dict = pk.load(fo, encoding='bytes') 
+        data_dict = _get_superclass(data_dict, CONST.SUPERCLASS_INDEX)
+        images = data_dict['data']
+        images = _normalize_pixel_values(images)
+        images = np.array(images)
+        labels = data_dict['fine_labels']
+        labels = _simplify_labels(labels, CONST.FINE_LABEL_NUMBERS)
+    return (images, labels)
 
-
-def get_superclass(data_dict, superclass_index):    
+def _get_superclass(data_dict, superclass_index):
     result = {'data': [], 'fine_labels':[]}
     for i in range(len(data_dict[b'coarse_labels'])): #
         if (data_dict[b'coarse_labels'][i] == superclass_index):
@@ -22,7 +28,7 @@ def get_superclass(data_dict, superclass_index):
     return result
 
 
-def normalize_pixel_values(image_list):
+def _normalize_pixel_values(image_list):
     result = []
     for img in image_list:
         norm_img = img/255
@@ -30,7 +36,7 @@ def normalize_pixel_values(image_list):
     return result
 
 
-def simplify_labels(train_labels, original_labels):
+def _simplify_labels(train_labels, original_labels):
     new_labels = np.zeros(len(train_labels))
     for i in range(len(train_labels)):
         for j in range(len(original_labels)):
@@ -38,3 +44,4 @@ def simplify_labels(train_labels, original_labels):
                 new_labels[i] = j
     return new_labels
     
+
--- main.py
+++ main.py
@@ -4,29 +4,24 @@ Image Classification Project
 """
 
 # import pandas as pd
-import numpy as np
+# import numpy as np
 # from tensorflow import keras
 
 import data_handler.handler as dh
 import configs.constants as CONST
 import models.tf_models as models
-# from utilities import visuals
+from utilities import visuals
 
 #%%
 
-train_dict = dh.importData(CONST.TRAIN_FILEPATH)
-train_dict = dh.get_superclass(train_dict, CONST.SUPERCLASS_INDEX)
-train_images = train_dict['data']
-train_images = dh.normalize_pixel_values(train_images)
-train_images = np.array(train_images)
-train_labels = train_dict['fine_labels']
-train_labels = dh.simplify_labels(train_labels, CONST.FINE_LABEL_NUMBERS)
 
+(train_images, train_labels) = dh.import_data(CONST.TRAIN_FILEPATH)
+(test_images, test_labels) = dh.import_data(CONST.TEST_FILEPATH)
 
 #%% Image visualization
 
-# img_num = 22
-# visuals.display_image(train_images[img_num])
+img_num = 69
+visuals.display_image(test_images[img_num])
 
 #%% Building Model
 # I will start using a keras model
