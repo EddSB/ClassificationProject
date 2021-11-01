94dd75ba466f2c32bdcc440f161b3ad09b8fe092
EddSB <eduardostimamiglio@gmail.com>
2021-11-01 08:08:09 -0300

    ADD: Created Module to hold global variables.

---
+++ configs/variables.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Module responsible for holding the project's global variables
+"""
+
+dataset_choice = 1       # 1 = CIFAR-10_People, 2 = CIFAR-100
+filter1 = 32
+filter2 = 64
+filter3 = 64
+epochs = 10
+augment_count = 4
--- main.py
+++ main.py
@@ -3,33 +3,20 @@
 Image Classification Project
 """
 
-# import pandas as pd
 import numpy as np
-# from tensorflow import keras
+import models.tf_models as models
 
 import data_handler.handler as dh
 import data_handler.augment as aug
-# import configs.constants as CONST
-import models.tf_models as models
 import utilities.visuals as vis
-# import utilities.utils as utils
+import configs.variables as var
 
 #%% 
 
-# Input Variables
-dataset_choice = 1       # 1 = CIFAR-10_People, 2 = CIFAR-100
-filter1 = 32
-filter2 = 64
-filter3 = 64
-epochs = 10
-augment_count = 4
-
-#%% 
-
-if (dataset_choice == 1):
+if (var.dataset_choice == 1):
     (train_images, test_images, 
      train_labels, test_labels) = dh.import_cifar100_people()
-elif (dataset_choice == 2):
+elif (var.dataset_choice == 2):
     (train_images, test_images, 
      train_labels, test_labels) = dh.import_cifar10()
 
@@ -38,13 +25,13 @@ elif (dataset_choice == 2):
 
 train_images, train_labels = aug.augment(train_images, 
                                          train_labels, 
-                                         augment_count)
+                                         var.augment_count)
 
 #%%
 
 model = models.Keras_convolutional()
 classes_count = len(np.unique(test_labels))
-model.build(classes_count, filter1, filter2, filter3)
+model.build(classes_count, var.filter1, var.filter2, var.filter3)
 
 #%%
 
@@ -52,7 +39,7 @@ model.compile_model()
 
 #%%
 
-history = model.train(train_images, train_labels, epochs)
+history = model.train(train_images, train_labels, var.epochs)
 
 #%%
 
